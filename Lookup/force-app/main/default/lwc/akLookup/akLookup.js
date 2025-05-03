74beb9bfb0f5a95d8d4d8983a9638160db93a197
Aditi <aditikulkarni7588@outlook.com>
2025-05-03 20:33:32 +0530

    6th Commit

--- Lookup/force-app/main/default/lwc/akLookup/akLookup.js
+++ Lookup/force-app/main/default/lwc/akLookup/akLookup.js
@@ -2,8 +2,10 @@ import { api, LightningElement, track } from 'lwc';
 
 export default class AkLookup extends LightningElement {
     // private variables
-    _activeTab = `All`;
+    _activeTab;
     _isComponentRendered = false;
+    _tabs;
+    _cancelBlur = false;
 
     @track results;
 
@@ -16,16 +18,74 @@ export default class AkLookup extends LightningElement {
         return this.results;
     }
 
+    @api
+    set tabs(value) {
+        this._tabs = value;
+        this._activeTab = this._tabs[0];
+    }
+
+    get tabs() {
+        return this._tabs;
+    }
+
     handleTabClick(e) {
-        //alert(JSON.stringify(e.target.innerText));
+        this._cancelBlur = true;
+        if (e.target.innerText !== this._activeTab) {
+            this.template.querySelectorAll(`.tab-span`).forEach((row) => {
+                if (row.innerText === this._activeTab || row.innerText === e.target.innerText) row.classList.toggle(`active`);
+            });
+            this._activeTab = e.target.innerText;
+        }
     }
 
     renderedCallback() {
-        if (!this._isComponentRendered) {
+        if (!this._isComponentRendered && this._activeTab) {
             this.template.querySelectorAll(`.tab-span`).forEach((row) => {
-                if (row.innerText === `All`) row.classList.toggle(`active`);
+                if (row.innerText === this._activeTab) row.classList.toggle(`active`);
             });
             this._isComponentRendered = true;
+        }
+    }
+
+    // input events
+    handleInputBlur(e) {
+        if (this._cancelBlur) return;
+        const element = this.template.querySelector(`.slds-combobox`);
+        if (element) {
+            this.toggleCss(element, `slds-is-open`, false);
+        }
+    }
+
+    handleInput(e) {
+        const element = this.template.querySelector(`.slds-combobox`);
+        if (element) {
+            this.toggleCss(element, `slds-is-open`, true);
+        }
+    }
+
+    handleInputClick(e) {
+        const element = this.template.querySelector(`.slds-combobox`);
+        if (element) {
+            this.toggleCss(element, `slds-is-open`, true);
+        }
+    }
+
+    toggleCss(element, cssClass, toggleValue) {
+        element.classList.toggle(cssClass, toggleValue);
+    }
+
+    handleMouseClick(e) {
+        if (e.type === `mousedown` && e.button === 0) {
+            this._cancelBlur = true;
+        }
+    }
+
+    handleRowClick(e) {
+        // alert(e.currentTarget.dataset.rowid);
+        this._cancelBlur = false;
+        const element = this.template.querySelector(`.slds-combobox`);
+        if (element) {
+            this.toggleCss(element, `slds-is-open`, false);
         }
     }
 }
--- Lookup/force-app/main/default/lwc/akRoot/akRoot.js
+++ Lookup/force-app/main/default/lwc/akRoot/akRoot.js
@@ -1,8 +1,13 @@
 import { LightningElement, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactCtrl.getContacts';
 
+//const TABS = [`All`, `Company`, `Industries and Contacts`];
+const TABS = [`A`, `B`, `C`, `D`];
+
 export default class AkRoot extends LightningElement {
     @track searchResults;
+    @track tabs = TABS;
+
     async connectedCallback() {
         const result = await getContacts({ searchText: '' });
         this.searchResults = result;
